a8372db0eb43f0af17899c7fc5cbc83455b71109
Rob Smyth <RobSmyth@users.noreply.github.com>
2025-05-07 14:22:47 +1000

    dev: WIP

---
+++ Git2SemVer.IntegrationTests/Framework/NullLogger.cs
@@ -0,0 +1,85 @@
+﻿using NoeticTools.Git2SemVer.Core.Logging;
+
+
+namespace NoeticTools.Git2SemVer.IntegrationTests.Framework;
+
+internal class NullLogger : Core.Logging.ILogger
+{
+    public void Dispose()
+    {
+    }
+
+    public IDisposable EnterLogScope()
+    {
+        return this;
+    }
+
+    public void Log(LoggingLevel level, string message)
+    {
+    }
+
+    public void LogDebug(string message)
+    {
+    }
+
+    public void LogDebug(Func<string> messageGenerator)
+    {
+    }
+
+    public void LogDebug(string message, params object[] messageArgs)
+    {
+    }
+
+    public void LogError(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LogError(string message, params object[] messageArgs)
+    {
+    }
+
+    public void LogError(Exception exception)
+    {
+    }
+
+    public void LogInfo(string message)
+    {
+    }
+
+    public void LogInfo(string message, params object[] messageArgs)
+    {
+    }
+
+    public void LogTrace(string message)
+    {
+    }
+
+    public void LogTrace(Func<string> messageGenerator)
+    {
+    }
+
+    public void LogTrace(string message, params object[] messageArgs)
+    {
+    }
+
+    public void LogWarning(string message)
+    {
+    }
+
+    public void LogWarning(string format, params object[] args)
+    {
+    }
+
+    public void LogWarning(Exception exception)
+    {
+    }
+
+    public string Errors => "";
+
+    public bool HasError => false;
+
+    public LoggingLevel Level { get; set; }
+
+    public string LogPrefix => "";
+}
---
+++ Git2SemVer.IntegrationTests/Framework/TestLoggerFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoeticTools.Git2SemVer.Core.Logging;
+using NoeticTools.Git2SemVer.Framework.Tools.CI;
+using NoeticTools.Git2SemVer.Testing.Core;
+
+
+namespace NoeticTools.Git2SemVer.IntegrationTests.Framework
+{
+    internal class TestLoggerFactory
+    {
+        public ILogger Create()
+        {
+            var teamcityHost = new TeamCityHost(new NullLogger());
+            if (teamcityHost.MatchesHostSignature())
+            {
+                Console.WriteLine("===1===");
+                Console.WriteLine("==== " + Environment.GetEnvironmentVariable("TMPDIR"));
+                var variables = Environment.GetEnvironmentVariables();
+                foreach (DictionaryEntry entry in variables)
+                {
+                    Console.WriteLine($"  === {entry.Key} = {entry.Value}");
+                }
+
+                //##teamcity[importData type='streamToBuildLog' filePath='path-to-file' wrapFileContentInBlock='false' charset='UTF-8']
+                return new ConsoleLogger();
+            }
+            else
+            {
+                return new NUnitLogger();
+            }
+        }
+    }
+}
